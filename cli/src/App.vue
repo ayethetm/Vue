1a5bf927f372d34074f5ab8df2c9ea42f3b95ba9
Aki25-mirai <62086934+Aki25-mirai@users.noreply.github.com>
2020-09-03 16:01:59 +0630

    update with cli

---
+++ cli/src/App.vue
@@ -0,0 +1,32 @@
+<!-- main component -->
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Testing msg="This is inheritanced data"></Testing>
+
+  </div>
+</template>
+
+<script>
+import Testing from './components/Testing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Testing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli/src/components/Testing.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>{{title}}</h1>
+		<p>{{msg}}</p>
+		<p>This is a testing for child components.</p>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		props:{
+			msg:null
+		},
+		data(){
+			return{
+				title:'Testing Vue'
+			}
+		}
+	}
+</script>
+
+<style type="text/css">
+	
+</style>
